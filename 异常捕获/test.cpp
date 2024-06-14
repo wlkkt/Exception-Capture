71a9f309b59b970d062ad1cfb3d3e6937b9fd0ad
wlkkt <yangyf0618@qq.com>
2024-06-14 23:15:40 +0800

    完善

--- 异常捕获/test.cpp
+++ 异常捕获/test.cpp
@@ -117,3 +117,14 @@ int main()
 	cout << "到这里了" << endl;
 	return 0;
 }
+
+
+#include "widget.h"
+#include <QApplication>
+int main(int argc, char* argv[])
+{
+	QApplication a(argc, argv); //定义并创建应用程序
+	Widget w; //定义并创建窗口
+	w.show(); //显示窗口
+	return a.exec(); //应用程序运行
+}
